91a65984b9e69e95d9720f78a4ea53cf3d4b7edd
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2022-12-03 09:23:16 -0500

    Adding boilerplate components

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PatientNotes from '../components/PatientNotes';
+import Drugs from '../components/Drugs';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+
+const Home = () => {}
+
+export default Home;
