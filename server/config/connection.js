f88c3075d12443f4180313a76be05f47be6078c7
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-12-08 19:59:04 -0500

    heroku push

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/PatientsPlease', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/patientsplease', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
